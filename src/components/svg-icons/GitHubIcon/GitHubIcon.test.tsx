e218da1359bf06261336eba97b4ac61cf4477de6
zavvdev <zaviryukha.igor99@gmail.com>
2021-12-09 10:44:25 +0200

    dev

--- src/components/svg-icons/GitHubIcon/GitHubIcon.test.tsx
+++
@@ -1,9 +0,0 @@
-import render from 'src/tests/utils/render';
-import GitHubIcon from 'src/components/svg-icons/GitHubIcon/GitHubIcon';
-
-describe('<GitHubIcon />', () => {
-  test('should render GitHubIcon component (snapshot)', () => {
-    const { asFragment } = render(<GitHubIcon />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
